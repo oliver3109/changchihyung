5bac49bfb1ef65fa1e952e70d083900c08bae60f
Oliver <810982048@qq.com>
2024-05-29 22:58:52 +0800

    fix bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  experimental: { appManifest: false },
+  experimental: { appManifest: false, payloadExtraction: true },
   modules: [
     "@nuxt/ui",
     "nuxt-icon",
