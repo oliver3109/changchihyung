f4eff1423d0ec653e6acdd8bb6c0f1eb8f6411fb
Oliver <810982048@qq.com>
2024-10-03 18:06:49 +0800

    fix bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,7 @@ export default defineNuxtConfig({
     },
   },
   i18n: {
-    strategy: "prefix_and_default", // 添加路由前缀的方式
+    strategy: "prefix", // 添加路由前缀的方式
     locales: ["en", "zh"], //配置语种
     defaultLocale: "en", // 默认语种
     vueI18n: "./i18n.config.ts", // 通过vueI18n配置
